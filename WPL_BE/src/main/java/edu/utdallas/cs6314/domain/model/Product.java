7ef61e4bf7a470d8a03fec9de677683c39054339
sathenikhil <nss150830@utdallas.edu>
2016-11-21 14:01:40 -0600

    Rejected commit: Default

---
+++ WPL_BE/src/main/java/edu/utdallas/cs6314/domain/model/Product.java
@@ -0,0 +1,56 @@
+package edu.utdallas.cs6314.domain.model;
+
+/**
+ * Created by niksat21 on 11/21/2016.
+ */
+public class Product {
+
+    String ProductID;
+    String Productname;
+    String ProductCategory;
+    String Price;
+    String Specification;
+
+
+    public String getProductID() {
+        return ProductID;
+    }
+
+    public void setProductID(String productID) {
+        ProductID = productID;
+    }
+
+    public String getProductname() {
+        return Productname;
+    }
+
+    public void setProductname(String productname) {
+        Productname = productname;
+    }
+
+    public String getProductCategory() {
+        return ProductCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        ProductCategory = productCategory;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getSpecification() {
+        return Specification;
+    }
+
+    public void setSpecification(String specification) {
+        Specification = specification;
+    }
+
+
+}
