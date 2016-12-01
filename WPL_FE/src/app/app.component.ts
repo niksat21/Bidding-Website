9710f329ac639caa8c040dcde9e2d19f7a06beaf
sathenikhil <nss150830@utdallas.edu>
2016-12-01 12:00:48 -0600

    I've added Login Service, please check if you can test this functionality

--- WPL_FE/src/app/app.component.ts
+++ WPL_FE/src/app/app.component.ts
@@ -1,20 +1,25 @@
 import {Component} from '@angular/core';
-import {LoginService} from "./services/Login/longin.service";
+
 import {User} from "./services/user";
 import {Login} from "./services/Login/login"
+import {Logout} from "./services/Logout/logout"
 
 // Add the RxJS Observable operators.
 import './rxjs-operators';
+import {UserService} from "./services/user.service";
+import {LoginService} from "./services/Login/longin.service";
+import {LogoutService} from "./services/Logout/logout.service";
 
 @Component({
   selector: 'app-root',
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [LoginService]
+  providers: [UserService,LogoutService,LoginService]
 })
 export class AppComponent {
   login : Login
-
+  user : User
+  logout : Logout
 
 }
