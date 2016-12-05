90c906e2a99a7062b9067246acfd7fce202437cb
Wyatt Lee Chastain <wyatt.chastain@slalom.com>
2016-12-04 18:33:29 -0600

    Fix new users.

---
+++ WPL_BE/src/main/java/edu/utdallas/cs6314/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package edu.utdallas.cs6314;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/resources/**").permitAll().anyRequest().authenticated().and()
+                .httpBasic();
+    }
+}
