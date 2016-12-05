0e446ce8a23ca79264619deab74497efe9bd4a90
sathenikhil <nss150830@utdallas.edu>
2016-12-04 18:00:23 -0600

    added new user reg and error component

---
+++ WPL_FE/src/app/components/error.component.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by niksat21 on 12/4/2016.
+ */
+/**
+ * Created by niksat21 on 12/3/2016.
+ */
+/**
+ * Created by niksat21 on 12/3/2016.
+ */
+
+/**
+ * Created by niksat21 on 12/2/2016.
+ */
+import {Component, NgModule} from '@angular/core';
+import {Validators, FormBuilder} from "@angular/forms";
+import { FormGroup, FormControl } from '@angular/forms';
+import {Http, Headers, RequestOptions} from '@angular/http';
+import {Router} from '@angular/router';
+
+@Component({
+
+
+  selector: 'app-root',
+  template : `
+            <nav-bar-before></nav-bar-before>
+            <br/><br/>
+            <h1>something went wrong.......</h1>
+            <div><img src = "../../assets/images/loginerror.png"/></div>
+`
+
+})
+
+export class ErrorComponent {
+
+
+}
